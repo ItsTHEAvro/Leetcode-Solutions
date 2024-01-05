114476b37c4eb63eb4c99153de95c8d1a71e3338
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-01-06 02:59:54 +0600

    Time: 14 ms (21.99%), Space: 8 MB (7.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,14 @@
 class Solution {
+private:
+    map<char, int> mp;
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return (s==t);
+        if(s.size()!=t.size()) return false;
+        for(char&c:s) mp[c]++;
+        for(char&c:t) mp[c]--;
+        for(auto&it:mp) {
+            if(it.second!=0) return false;
+        }
+        return true;
     }
 };
