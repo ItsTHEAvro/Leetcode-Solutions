9f15ef700af314bdd90241f619a42bca7d5c1fde
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-04-13 02:02:05 +0600

    Time: 4 ms (93.99%), Space: 22.6 MB (43.00%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,19 +4,19 @@ public:
         
         int n = height.size();
         
-        vector<int> lmax(n);
+        int lmax[n];
         lmax[0] = height[0];
         for(int i=1; i<n; i++) {
             lmax[i] = max(lmax[i-1],height[i]);
         }
         
-        vector<int> rmax(n);
+        int rmax[n];
         rmax[n-1] = height[n-1];
         for (int i=n-2; i>=0; i--) {
             rmax[i] = max(rmax[i+1],height[i]);
         }
         
-        vector<int> minmax(n);
+        int minmax[n];
         for(int i=0; i<n; i++) {
             minmax[i]= min(lmax[i],rmax[i]);
         }
