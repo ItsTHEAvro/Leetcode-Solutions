ed9c74f7f27ef6c1de5e9f16c808125ca69a2ba7
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-04-15 19:44:29 +0600

    Time: 0 ms (100.00%), Space: 11.1 MB (31.35%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -12,11 +12,11 @@
 class Solution {
 public:
     int dfs(TreeNode* root, int sum) {
-        if(root == nullptr) return 0;
+        if(!root) return 0;
         
         sum = sum*10 + root->val;
         
-        if(root->left == nullptr && root->right == nullptr) return sum;
+        if(!root->left && !root->right) return sum;
         
         return dfs(root->left, sum) + dfs(root->right, sum);
     }
