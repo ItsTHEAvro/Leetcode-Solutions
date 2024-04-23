a7c0eb5fb8406489a59af8a791fa73bb65509709
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-04-23 07:15:40 +0600

    Time: 0 ms (100.00%), Space: 8.3 MB (82.33%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int numberOfSpecialChars(string word) {
         int lower[26] = {0}, upper[26] = {0}, cnt = 0;
-        for(char ch:word) {
-            if(ch >= 'a' && ch <= 'z') {
-                lower[ch - 'a'] = 1;
+        for(int i=0; i<word.size(); i++) {
+            if(word[i] >= 'a' && word[i] <= 'z') {
+                lower[word[i] - 'a'] = 1;
             }
             else {
-                upper[ch - 'A'] = 1;
+                upper[word[i] - 'A'] = 1;
             }
         }
         
