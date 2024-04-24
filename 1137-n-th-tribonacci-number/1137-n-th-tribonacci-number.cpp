3d2b5a8cc8f65d08b81a906b3078d206d9692bb9
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-04-24 17:55:29 +0600

    Time: 0 ms (100.00%), Space: 7.3 MB (13.08%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -4,15 +4,14 @@ public:
         if(n == 0) return 0;
         else if(n == 1 || n == 2) return 1;
         
-        int t1 = 0, t2 = 1, t3 = 1, tn;
+        int *dp = (int*) calloc(n+1, sizeof(int));
+        
+        dp[1] = 1, dp[2] = 1;
         
         for(int i = 3; i <= n; i++) {
-            tn = t1+ t2+ t3;
-            t1 = t2;
-            t2 = t3;
-            t3 = tn;
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
         
-        return tn;
+        return dp[n];
     }
 };
