348cd05eadf69a7cf2ba830e36d4c7a4ca54b8d3
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-05-03 08:16:47 +0600

    Time: 3 ms (39.03%), Space: 7.5 MB (36.10%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        vector<int> v1, v2;
-        
         int n = version1.size(), m = version2.size(), i = 0, j = 0;
         
         while(i < n || j < m) {
-            int tmp = 0;
+            long int tmp1 = 0, tmp2 = 0;
             
             while(i < n && version1[i] != '.') {
-                tmp = tmp * 10 + (version1[i] - '0');
+                tmp1 = tmp1 * 10 + (version1[i] - '0');
                 i++;
             }
             
-            v1.push_back(tmp);
             i++;
             
-            tmp = 0;
-            
             while(j < m && version2[j] != '.') {
-                tmp = tmp * 10 + (version2[j] - '0');
+                tmp2 = tmp2 * 10 + (version2[j] - '0');
                 j++;
             }
             
-            v2.push_back(tmp);
             j++;
-        }
-        
-        for(i = 0; i < min(v1.size(), v2.size()); i++) {
-            if(v1[i] > v2[i]) return 1;
-            if(v1[i] < v2[i]) return -1;
+            
+            if (tmp1 > tmp2) return 1;
+            if (tmp1 < tmp2) return -1;
         }
         
         return 0;
