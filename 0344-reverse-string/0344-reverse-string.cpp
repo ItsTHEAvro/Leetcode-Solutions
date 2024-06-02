9af431b7e610f26b79d6b1bebaff47574a5d3c05
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-06-02 06:37:27 +0600

    Time: 18 ms (53.67%), Space: 26.9 MB (60.22%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,7 +4,9 @@ public:
         int l = 0, r = s.size() - 1;
         
         while(l < r) {
-            swap(s[l], s[r]);
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
             l++;
             r--;
         }
