d21c0114ec2e86633125f8a5eac593797afa9b9d
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-06-21 20:11:28 +0600

    Time: 7 ms (50.11%), Space: 17.7 MB (44.48%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        
+        for(string operation : operations) {
+            if(operation == "++X" || operation == "X++") x++;
+            else x--;
+        }
+        
+        return x;
+    }
+};
