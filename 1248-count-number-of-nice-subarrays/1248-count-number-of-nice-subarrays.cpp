013d6a41566d9fb4aee025cf0fd90f85f4e72748
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-06-22 09:16:43 +0600

    Time: 91 ms (74.98%), Space: 70 MB (36.89%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int res = 0, odd = 0;
-        int l = 0, m = 0, r = 0;
         
-        for(; r<nums.size(); r++) {
+        // Three pointer sliding window        
+        // Shift right pointer
+        for(int r = 0, l = 0, m = 0; r<nums.size(); r++) {
+            
+            // If encountered number is odd, increment odd
             if(nums[r]&1) {
                 odd++;
             }
             
+            // If odd > k shift left pointer to the point where odd is not greater than k
             while(odd > k) {
                 if(nums[l]&1) {
                     odd--;
@@ -18,6 +22,7 @@ public:
                 m = l;
             }
             
+            // If odd == k move the middle pointer to the first odd number in the window and increase count
             if(odd == k) {
                 while(!(nums[m]&1)) {
                     m++;
