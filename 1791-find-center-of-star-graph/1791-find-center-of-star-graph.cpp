e54a10cf527cfb2183ba5cd6ca69fbda48f56209
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-06-27 14:14:41 +0600

    Time: 144 ms (44.00%), Space: 70.8 MB (77.19%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& e) {
-        if (e[0][0] == e[1][0] || e[0][0] == e[1][1]) return e[0][0]; 
-        else return e[0][1];
+        if (e[0][0] == e[1][0] || e[0][0] == e[1][1]) {
+            return e[0][0];
+        } 
+        else {
+            return e[0][1];
+        }
     }
 };
