511041eb1d63108744fb2e614e65de9ea816c012
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-06-28 11:09:58 +0600

    Time: 266 ms (74.66%), Space: 119.8 MB (72.59%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        // Array to store the degree of each node
         vector<int> degree(n, 0);
         
-        // Calculating the degree of each node
         for(const auto& road:roads) {
             degree[road[0]]++;
             degree[road[1]]++;
         }
         
-        // Sorting the array in decreasing order 
-        // in order to maximize the importance of the node with the highest degree
         sort(degree.rbegin(), degree.rend());
         
         long long ans = 0;
         
-        // Calculating maximum total importance
         for(int i=0; i<n; i++) {
             if(degree[i]) {
                 ans += (long long)(n-i) * degree[i];
             } else {
-                // If degree of a node doesn't exist, break the loop
                 break;
             }
         }
