4747aca1dbf3e4e034903bb311993ff58801652b
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-06-29 11:33:20 +0600

    Time: 107 ms (82.77%), Space: 66.1 MB (78.38%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -15,8 +15,8 @@ public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         vector<vector<int>> ans(n), graph(n);
         
-        for(auto &v:edges){
-            graph[v[0]].push_back(v[1]);
+        for(const auto &edge:edges){
+            graph[edge[0]].push_back(edge[1]);
         }
         
         for(int i=0; i<n; i++){
