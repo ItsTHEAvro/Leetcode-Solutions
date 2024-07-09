dd88e16ad491d4ed553ab301db09e3114a7f0838
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-07-09 07:57:48 +0600

    Time: 151 ms (55.88%), Space: 76.5 MB (57.65%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
-    double averageWaitingTime(vector<vector<int>>& customers) {
-        /*
-        cur = the current time
-        t = customer arrival time
-        d = time needed to prepare
-        
-        for each customer,
-            start preparing for order at max(cur, t)
-                finish his order at max(cur, t) + d
-                    waiting period max(cur, t) + d - t
-        */
-        
+    double averageWaitingTime(vector<vector<int>>& customers) {       
         double totalWait = 0, cur = 0;
         
-        for (const vector<int>& customer: customers) {
-            int t = customer[0];
-            int d = customer[1];
-            
-            cur = max(cur, 1.0 * t) + d;
-            totalWait += cur - t;
+        for (const vector<int>& customer: customers) {         
+            cur = max(cur, 1.0 * customer[0]) + customer[1];
+            totalWait += cur - customer[0];
         }
         
         return totalWait / customers.size();
