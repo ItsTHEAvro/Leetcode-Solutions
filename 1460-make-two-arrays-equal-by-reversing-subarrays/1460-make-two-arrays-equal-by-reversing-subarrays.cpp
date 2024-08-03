a53c90a321234a2caf959d8b3362b981dbe137d4
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2024-08-03 14:56:36 +0600

    Time: 13 ms (29.25%), Space: 20.2 MB (13.01%) - LeetHub

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        map<int, int> mp;
+        
+        for(int i=0; i < target.size(); i++) {
+            mp[target[i]]++;
+            mp[arr[i]]--;
+        }
+        
+        for(const auto item:mp) {
+            if(item.second) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
