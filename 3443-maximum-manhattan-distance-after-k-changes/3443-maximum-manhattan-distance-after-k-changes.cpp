2507bd9ad7e1eabbd97ee4b1071fc98c9eed9fa8
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2025-06-20 11:07:20 +0600

    Time: 43 ms (94.55%), Space: 38.2 MB (62.42%)

---
+++ 3443-maximum-manhattan-distance-after-k-changes/3443-maximum-manhattan-distance-after-k-changes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxDistance(string s, int k) {
+        int lat = 0, lon = 0, ans = 0;
+        int n = s.size();
+        
+		for(int i = 0; i < n; i++) {
+            switch (s[i]) {
+                case 'N':
+                    lat++;
+                    break;
+                case 'S':
+                    lat--;
+                    break;
+                case 'E':
+                    lon++;
+                    break;
+                case 'W':
+                    lon--;
+                    break;
+            }
+            
+			ans = max(ans, min(abs(lat) + abs(lon) + k * 2, i + 1));
+        }
+		
+        return ans;
+    }
+};
